0572ac76076f6e7025fe50e7959725c6e43f428c
Francesco Bianco <info.francescobianco@gmail.com>
2022-07-19 18:27:38 +0200

    fix

--- tests/ErrorsTest.php
+++ tests/ErrorsTest.php
@@ -6,15 +6,18 @@ use PHPUnit\Framework\Error\Warning;
 
 class ErrorsTest extends ImapTestCase
 {
-    public function testLastError()
+    public function testLastError1()
     {
         $this->expectException(Warning::class);
-        $this->expectExceptionMessage('Warning: imap2_open(): Couldn\'t open stream ... in '.__FILE__.' on line '.(__LINE__ + 1));
+        $this->expectExceptionMessage('imap2_open(): Couldn\'t open stream ... in ' . __FILE__ . ' on line ' . (__LINE__ + 1));
         imap2_open('...', '...', '...', OP_XOAUTH2);
         $this->assertEquals('Can\'t open mailbox ...: no such mailbox', imap2_last_error());
+    }
 
+    public function testLastError2()
+    {
         $this->expectException(Warning::class);
-        $this->expectExceptionMessage('Warning: imap2_open(): Couldn\'t open stream {imap.gmail.com:993/imap/ssl} in '.__FILE__.' on line '.(__LINE__ + 1));
+        $this->expectExceptionMessage('imap2_open(): Couldn\'t open stream {imap.gmail.com:993/imap/ssl} in '.__FILE__.' on line '.(__LINE__ + 1));
         imap2_open('{imap.gmail.com:993/imap/ssl}', 'wrong-username', 'wrong-password', OP_XOAUTH2);
         $this->assertEquals('Can not authenticate to IMAP server: [AUTHENTICATIONFAILED] Invalid credentials (Failure)', imap2_last_error());
     }
