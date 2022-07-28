4503ab85a90fd94005f712948da0ad82487a2526
Francesco Bianco <info.francescobianco@gmail.com>
2022-07-28 13:20:37 +0200

    fix

--- tests/CleaningTest.php
+++ tests/CleaningTest.php
@@ -10,7 +10,7 @@ class CleaningTest extends ImapTestCase
     {
         $imap = imap2_open($this->mailbox, $this->username, $this->accessToken, OP_XOAUTH2);
 
-        $deleteRule = '/^(\d+|test_|mailbox_)/';
+        $deleteRule = '/^(\d+|test_|mailbox_|Mailbox )/';
 
         $mailboxes = imap2_getmailboxes($imap, $this->mailbox, '*');
         foreach ($mailboxes as $mailbox) {
