9fbb23c747dba40d287fe78572aa5ade86893980
francescobianco <info.francescobianco@gmail.com>
2022-08-26 21:34:44 +0200

    fix imap_mailboxmsginfo

--- .github/workflows/refresh-access-token.sh
+++ .github/workflows/refresh-access-token.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-curl \
-  -X GET \
-  -H "Accept: application/vnd.github+json" \
-  -H "Authorization: token $GITHUB_TOKEN" \
-  https://api.github.com/repos/javanile/php-imap2/actions/secrets/GOOGLE_ACCESS_TOKEN
+#curl \
+#  -X GET \
+#  -H "Accept: application/vnd.github+json" \
+#  -H "Authorization: token $GITHUB_TOKEN" \
+#  https://api.github.com/repos/javanile/php-imap2/actions/secrets/GOOGLE_ACCESS_TOKEN
 
+gh secret set GOOGLE_ACCESS_TOKEN --org javanile --repos php-imap2 --body HELLO
 
