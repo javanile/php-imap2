c4e3205198a0edd9d4ee3c73bb820f57b7072d59
francescobianco <info.francescobianco@gmail.com>
2022-08-28 13:08:49 +0200

    fix imap_mailboxmsginfo

--- contrib/refresh-access-token.sh
+++ contrib/refresh-access-token.sh
@@ -8,13 +8,18 @@
 ## ======
 
 ## Refresh the access token
-access_token=$(curl -s \
+refresh_request=$(curl -s \
   -X POST \
   -d "client_id=$GOOGLE_CLIENT_ID" \
   -d "client_secret=$GOOGLE_CLIENT_SECRET" \
   -d "refresh_token=$GOOGLE_REFRESH_TOKEN" \
   -d 'grant_type=refresh_token' \
-  https://accounts.google.com/o/oauth2/token | grep "access_token" | cut -d'"' -f4)
+  https://accounts.google.com/o/oauth2/token)
+access_token=$(echo "$refresh_request" | grep "access_token" | cut -d'"' -f4)
 
 ## Store newly access token
-gh secret set GOOGLE_ACCESS_TOKEN --body $access_token --repo javanile/php-imap2
+if [ -n "$access_token" ]; then
+  gh secret set GOOGLE_ACCESS_TOKEN --body $access_token --repo javanile/php-imap2
+else
+  echo "Problem on refresh access token: $refresh_request"
+fi
